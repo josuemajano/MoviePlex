3e4e21be42b42b436168945a83e9fef2cb522104
Josue Majano <josue_majano@yahoo.com>
2021-02-23 19:23:30 -0600

    created route to add new user in database

---
+++ server/controllers/userController.js
@@ -0,0 +1,24 @@
+const db = require('../models/');
+const User = db.User;
+const Sequelize = require('sequelize');
+var userHandeler = require('../routes/userHandler');
+
+
+// Get all User in database
+
+
+
+// Create new User 
+
+exports.createUser = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    const newUser = await User.create({
+        firstName,
+        lastName,
+        email,
+        password
+    });
+    res.json({
+        id: newUser.id
+    });
+}
--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const parseurl = require('parseurl');
 const Sequelize = require('sequelize');
+const { User } = require('./models');
+const userHandler = require('./routes/userHandler');
 
-const PORT = 3001;
+const PORT = 4000;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', userHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
