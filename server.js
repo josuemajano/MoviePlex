82d9e24b5ddb1de0f4a36c50b7e13a76847c3a8a
Josue Majano <josue_majano@yahoo.com>
2021-03-04 14:20:15 -0600

    fixing deploy problems

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use('/', userHandler);
 
 // Catch all route 
 app.get('*', (req, res) => {
-    res.status(404).send('No Content Found')
-});
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  }); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
